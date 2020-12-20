e2852ef9d51111b2548bb5be36c193ab2639d203
woodvase <yongcai.liu@outlook.com>
2020-12-20 11:29:01 -0800

    refactor the code a bit

--- calc1.py
+++ calc1.py
@@ -12,23 +12,24 @@ class Token:
     def __init__(self, type, value):
         self.type = type
         self.value = value
+
     def __str__(self):
         return 'Token({type}, {value})'.format(
             type = self.type,
             value = repr(self.value)
         )
+
     def __repr__(self):
         return self.__str__()
 
-class Interpreter:
+class Lexer(object):
     def __init__(self, text):
         self.text = text
         self.pos = 0
-        self.current_token = None
         self.current_char = self.text[self.pos]
-    
+
     def error(self):
-        raise Exception('Invalid syntax')
+        raise Exception('Invalid charactor')
 
     def advance(self):
         self.pos += 1
@@ -37,6 +38,10 @@ class Interpreter:
         else:
             self.current_char = None
 
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
     def integer(self):
         result = ''
         while self.current_char is not None and self.current_char.isdigit():
@@ -44,17 +49,13 @@ class Interpreter:
             self.advance()
         return int(result)
 
-    def skip_whitespace(self):
-        while self.current_char is not None and self.current_char.isspace():
-            self.advance()
-
     def get_next_token(self):
         while self.current_char is not None:
 
             if self.current_char.isspace():
                 self.skip_whitespace()
                 continue
-            
+
             if self.current_char.isdigit():
                 return Token(INTEGER, self.integer())
 
@@ -69,29 +70,36 @@ class Interpreter:
             self.error()
         return Token(EOF, None)
 
+class Interpreter:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise Exception('Invalid syntax')
 
     def eat(self, token_type):
         if self.current_token.type == token_type:
-            self.current_token = self.get_next_token()
+            self.current_token = self.lexer.get_next_token()
         else:
             self.error()
 
-    def term(self):
+    def factor(self):
         token = self.current_token
         self.eat(INTEGER)
         return token.value
 
     def expr(self):
-        self.current_token = self.get_next_token()
-        result = self.term()
+        result = self.factor()
+
         while self.current_token.type in (PLUS, MINUS):
             token = self.current_token
             if token.type == PLUS:
                 self.eat(PLUS)
-                result += self.term()
+                result += self.factor()
             elif token.type == MINUS:
                 self.eat(MINUS)
-                result -= self.term()
+                result -= self.factor()
 
         return result
 
@@ -103,7 +111,8 @@ def main():
             break
         if not text:
             continue
-        interpreter = Interpreter(text)
+        lexer = Lexer(text)
+        interpreter = Interpreter(lexer)
         result = interpreter.expr()
         print(result)
 
